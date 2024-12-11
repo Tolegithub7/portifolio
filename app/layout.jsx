ecf302eabba5601fc1637cfec842fd83bbad63b6
Tolosa <132619843+Tolegithub7@users.noreply.github.com>
2024-12-11 13:41:47 +0300

    ohhu

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,6 @@
 import { JetBrains_Mono } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/Header";
 
 const jetbrainsMono = JetBrains_Mono({
   subsets: ["latin"],
@@ -24,7 +25,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={jetbrainsMono.variable}>{children}</body>
+      <body className={jetbrainsMono.variable}>
+        <Header />
+        {children}
+      </body>
     </html>
   );
 }
