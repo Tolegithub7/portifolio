11ca8a003118c9b7b29ab39e0049fd73eafdc37d
Tolosa <132619843+Tolegithub7@users.noreply.github.com>
2024-12-11 09:36:12 +0300

    tailwind config update

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,6 +6,7 @@ const config = {
 		"./components/**/*.{js,ts,jsx,tsx,mdx}",
 		"./app/**/*.{js,ts,jsx,tsx,mdx}",
 	],
+	prefix: '',
 	theme: {
 		extend: {
 			colors: {
@@ -16,6 +17,20 @@ const config = {
 				lg: 'var(--radius)',
 				md: 'calc(var(--radius) - 2px)',
 				sm: 'calc(var(--radius) - 4px)'
+			},
+			keyframes: {
+				"accordion-down": {
+					from: { height: "0" },
+					to: { height: "var(--radix-accordion-content-height)" },
+				},
+				"accordion-up": {
+					from: { height: "var(--radix-accordion-content-height)" },
+					to: { height: "0" },
+				}
+			},
+			animation: {
+				"accordion-down": "accordion-down 0.2s ease-out",
+				"accordion-up": "accordion-up 0.2s ease-out",
 			}
 		},
 		screens: {
@@ -23,6 +38,10 @@ const config = {
 			md: '768px',
 			lg: '960px',
 			xl: '1200px'
+		},
+		container: {
+			center: true,
+			padding: '15px'
 		}
 	},
 	plugins: [require("tailwindcss-animate")],
