c1b39a2d5107bab20ae79000b64e4b0663ee10bb
Tolosa <132619843+Tolegithub7@users.noreply.github.com>
2024-12-19 16:05:53 +0300

    skills, experience and education list added

---
+++ app/resume/page.jsx
@@ -0,0 +1,137 @@
+"use client";
+
+import { Description } from '@radix-ui/react-dialog';
+import { 
+  FaHtml5, 
+  FaCss3, 
+  FaJs, 
+  FaReact, 
+  FaPhp, 
+  FaLaravel, 
+  FaDatabase  // MySQL
+} from 'react-icons/fa';
+import { 
+  SiAdobecreativecloud,
+  SiTailwindcss,
+  SiNextdotjs,
+  SiCapcut, // CapCut 
+  SiDavinciresolve // DaVinci Resolve 
+} from 'react-icons/si';
+
+const about = {
+  title: 'About Me',
+  Description: 'I am a developer with a passion for creating engaging and user-friendly applications. My expertise lies in building efficient, responsive, and accessible web applications using HTML, CSS, JavaScript, and frameworks like React, Next.js, and Laravel. I also have experience with various database management systems, such as MySQL and MongoDB, and have worked with various design tools, such as Adobe Creative Cloud, Tailwind CSS, and CapCut. I am always eager to learn and grow, and I am excited to contribute to the community as a developer.',
+  info: [
+    {
+      fieldName: "Name",
+      fieldValue: "Tolosa Diriba"
+    }, {
+      fieldName: "Phone",
+      fieldValue: "+251955251753"
+    }, {
+      fieldName: "Experience",
+      fieldValue: "+2 Years"
+    }, {
+      fieldName: "Nationality",
+      fieldValue: "Ethiopian"
+    }, {
+      fieldName: "Email Address",
+      fieldValue: "tolekadima7@gmail.com"
+    }, {
+      fieldName: "Github",
+      fieldValue: "https://github.com/tolekadima7"
+    }, {
+      fieldName: "Freelance",
+      fieldValue: "Available"
+    }, {
+      fieldName: "Address",
+      fieldValue: "Addis Ababa, Ethiopia"
+    }, {
+      fieldName: "Languages",
+      fieldValue: "English, Amharic"
+    }
+  ]
+}
+//Experience Data
+const experiences = {
+    icon: "/assets/resum/badge.svg",
+    title: "My Experience",
+    Description: "I am a developer with a passion for creating engaging and user-friendly applications. My expertise lies in building efficient, responsive, and accessible web applications using HTML, CSS, JavaScript, and frameworks like React, Next.js, and Laravel. I also have experience with various database management systems, such as MySQL and MongoDB, and have worked with various design tools, such as Adobe Creative Cloud, Tailwind CSS, and CapCut. I am always eager to learn and grow, and I am excited to contribute to the community as a developer.",
+    items: [
+      {
+        company: "Microsoft",
+        position: "Software Engineer",
+        duration: "2020 - Present",
+      }
+    ]
+};
+//Education Data
+const eductaion = {
+  icon: "/assets/resum/cap.svg",
+  title: "My Education",
+  Description: "I am a developer with a passion for creating engaging and user-friendly applications. My expertise lies in building efficient, responsive, and accessible web applications using HTML, CSS, JavaScript, and frameworks like React, Next.js, and Laravel.",
+  items: [
+    {
+      institution: "Addis Ababa University",
+      degree: "Bs. in Information System",
+      duration: "2021 - Present",
+    }, {
+      institution: "Cisco",
+      degree: "CCNA",
+      duration: "2017 - 2020",
+    }
+  ]
+};
+//Skills Data
+const skills = {
+  title: "My Skills",
+  Description: "I am a developer with a passion for creating engaging and user-friendly applications. My expertise lies in building efficient, responsive, and accessible web applications using HTML, CSS, JavaScript, and frameworks like React, Next.js, and Laravel.",
+  skillList: [
+    {
+      icon: <FaHtml5 />,
+      name: "html 5"
+    }, {
+      icon: <FaCss3 />,
+      name: "css 3"
+    }, {
+      icon: <FaJs />,
+      name: "javascript"
+    }, {
+      icon: <FaReact />,
+      name: "react"
+    }, {
+      icon: <FaPhp />,
+      name: "php"
+    }, {
+      icon: <FaLaravel />,
+      name: "laravel"
+    }, {
+      icon: <FaDatabase />,
+      name: "mysql"
+    }, {
+      icon: <SiAdobecreativecloud />,
+      name: "adobecreative cloud"
+    }, {
+      icon: <SiTailwindcss />,
+      name: "tailwind css"
+    }, {
+      icon: <SiNextdotjs />,
+      name: "next.js"
+    }, {
+      icon: <SiCapcut />,
+      name: "capcut"
+    }, {
+      icon: <SiDavinciresolve />,
+      name: "davinci resolve"
+    }
+  ]
+}
+const Resume = () => {
+  return (
+    <div>
+      my Resume
+    </div>
+  )
+}
+
+export default Resume;
