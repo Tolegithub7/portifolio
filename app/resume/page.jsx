25627a6b223e2d3e1130eb45ddcd571cbca2c4c6
Tolosa <132619843+Tolegithub7@users.noreply.github.com>
2024-12-25 10:49:08 +0300

    fixing bug

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -124,10 +124,11 @@ const skills = {
   ]
 }
 
-import { Tabs, TabsTrigger, TabsList } from '@/components/ui/tabs';
+import { Tabs, TabsTrigger, TabsList, TabsContent } from '@/components/ui/tabs';
 // import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/components/ui/tooltip';
 
 // import { ScrollArea } from '@/components/ui/scroll-area';
+import { ScrollArea, ScrollBar } from '@/components/ui/scroll-area';
 import { motion } from 'framer-motion';
 
 const Resume = () => {
@@ -149,32 +150,54 @@ const Resume = () => {
           {/* content  */}
           <div className='min-h-[70vh] w-full'>
             {/* experience  */}
-            <TabsContent value="experience" className='w-full'>
+            <TabsContent value="education" className='w-full'>
               <div className="flex flex-col gap-[30px] text-center xl:text-left">
                 <h3 className="text-4xl font-bold">{experiences.title}</h3>
                 <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">{experiences.description}</p>
                 <ScrollArea className="h-[400px] ">
                   <ul className='grid grid-cols-1 lg:grid-cols-2 gap-[30px]'>
-                    {experiences.items.map((item, index) => {
-                      return (
-                        <li key={index} className='bg-[#232329] h-[184px] py-6 px-10 rounded-xl flex flex-col justify-center items-center lg:items-start gap-1'>
-                          <span className='text-accent '>{item.duration}</span>
-                          <h3 className='text-xl max-w-[260px] min-h-[60px] text-center lg:text-left '>{item.position}</h3>
-                          <div className='flex items-center gap-3'>
-                            {/* dot  */}
-                            <span className='w-[6px] h-[6px] rounded-full bg-accent'></span>
-                            <p className='text-white/60 '>{items.company}</p>
-                          </div>
+                    {experiences.items?.length > 0 ? (
+                      experiences.items.map((item, index) => (
+                        <li
+                          key={index}
+                          className='bg-[#232329] h-[184px] py-6 px-10 rounded-xl flex flex-col justify-center items-center lg:items-start gap-1'
+                        >
+                          <span className='text-accent'>{item.duration}</span>
+                          <strong>{item.position}</strong>
+                          <em>{item.company}</em>
                         </li>
-                      )
-                    })}
+                      ))
+                    ) : (
+                      <li>No experience data available</li>
+                    )}
                   </ul>
                 </ScrollArea>
               </div>
             </TabsContent>
             {/* Education  */}
             <TabsContent value="education" className='w-full'>
-              Education
+            <div className="flex flex-col gap-[30px] text-center xl:text-left">
+                <h3 className="text-4xl font-bold">{eductaion.title}</h3>
+                <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">{eductaion.Description}</p>
+                <ScrollArea className="h-[400px] ">
+                  <ul className='grid grid-cols-1 lg:grid-cols-2 gap-[30px]'>
+                    {experiences.items?.length > 0 ? (
+                      experiences.items.map((item, index) => (
+                        <li
+                          key={index}
+                          className='bg-[#232329] h-[184px] py-6 px-10 rounded-xl flex flex-col justify-center items-center lg:items-start gap-1'
+                        >
+                          <span className='text-accent'>{item.duration}</span>
+                          <strong>{item.position}</strong>
+                          <em>{item.company}</em>
+                        </li>
+                      ))
+                    ) : (
+                      <li>No Education data available</li>
+                    )}
+                  </ul>
+                </ScrollArea>
+              </div>
             </TabsContent>
             {/* Skills  */}
             <TabsContent value="skills" className='w-full'>
